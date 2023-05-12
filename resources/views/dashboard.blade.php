833e4936118ac375858aaa1f86bc805b9f22c145
satianferdy <satianferdy@gmail.com>
2023-05-12 22:17:03 +0700

    create home section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,8 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Font Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -24,7 +26,7 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-fixed w-100">
         <div class="container">
             <a class="navbar-brand" href="#">The Kost</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -56,12 +58,37 @@
                     </li>
                 </ul>
                 <div>
-                    <a href="#" class="btn btn-secondary">SIGN UP</a>
+                    {{-- <a href="#" class="btn btn-secondary">SIGN UP</a> --}}
                     <a href="#" class="btn btn-primary">LOGIN</a>
                 </div>
             </div>
         </div>
     </nav>
+
+    <!-- home section -->
+    <section id="home">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-md-6 my-auto home-tagline">
+                    <h1>Cari Kos Sambil Rebahan
+                        Dengan <span>The Kost.</span></h1>
+                    <p><span class="fw-bold">Cari kosan</span> yang aman, tenang, dan terjangkau? Kami punya
+                        solusinya! Kosan kami memiliki lingkungan yang aman dan
+                        tenang untuk Anda tinggali.</p>
+
+                    <button class="button-lg-primary">For more information</button>
+                    <a href="#">
+                        <img src="{{ asset('assets/img/arrow-right.png') }}" alt="">
+                    </a>
+                </div>
+            </div>
+
+            <img src="{{ asset('assets/img/home.png') }}" alt="" class="home-img">
+            <img src="{{ asset('assets/img/layer.png') }}" alt=""
+                class="accsent-img h-100 position-absolute top-0 start-0">
+
+        </div>
+    </section>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
